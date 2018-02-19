601fefea550b2c07368ec880a1c4b2020cf29e75
Trần Đức Tiến <ductienas@gmail.com>
2018-02-19 17:17:23 +0700

    payment flow

--- models/lesson.js
+++ models/lesson.js
@@ -1,11 +1,11 @@
-/*--------------------------------------------------------
- * Author Ngo An Ninh
- * Email ninh.uit@gmail.com
- * Phone 0978 108 807
- *
- * Created: 2018-01-10 23:20:59
- *-------------------------------------------------------*/
+/* --------------------------------------------------------
+* Author Trần Đức Tiến
+* Email ductienas@gmail.com
+* Phone 0972970075
+*
+* Created: 2018-02-19 12:52:04
+*------------------------------------------------------- */
 
-export default function (/* Lesson */) {
-
+export default function (Lesson) {
+	Lesson.validatesUniquenessOf('name');
 }
--- models/level.js
+++ models/level.js
@@ -1,10 +1,10 @@
-/*--------------------------------------------------------
- * Author Ngo An Ninh
- * Email ninh.uit@gmail.com
- * Phone 0978 108 807
- *
- * Created: 2018-01-10 23:20:59
- *-------------------------------------------------------*/
+/* --------------------------------------------------------
+* Author Trần Đức Tiến
+* Email ductienas@gmail.com
+* Phone 0972970075
+*
+* Created: 2018-02-19 12:51:52
+*------------------------------------------------------- */
 
 export default function (Level) {
 	Level.validatesUniquenessOf('name');
